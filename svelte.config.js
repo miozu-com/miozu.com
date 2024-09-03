d53e4904aad34de153f03f3534d4f6c7e33259f5
Nicholas Glazer <glazer.nicholas@gmail.com>
2024-09-03 17:43:36 +0000

    adapter static

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,24 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-static';
+//import adapter from '@sveltejs/adapter-cloudflare';
 import {vitePreprocess} from '@sveltejs/vite-plugin-svelte';
 import {preprocessMeltUI, sequence} from '@melt-ui/pp';
 
 const config = {
  extensions: ['.svelte', '.md'],
  kit: {
-  adapter: adapter({
-   routes: {
-    include: ['/*'],
-    exclude: ['<all>']
-   },
-   platformProxy: {
-    configPath: 'wrangler.toml',
-    environment: undefined,
-    experimentalJsonConfig: false,
-    persist: false
-   }
-  })
+  // adapter: adapter({
+  //  routes: {
+  //   include: ['/*'],
+  //   exclude: ['<all>']
+  //  },
+  //  platformProxy: {
+  //   configPath: 'wrangler.toml',
+  //   environment: undefined,
+  //   experimentalJsonConfig: false,
+  //   persist: false
+  //  }
+  // })
+  adapter: adapter()
  },
  preprocess: sequence([
   vitePreprocess(),
